92ba0f96a61091d3266766c237d1dd33f8fa12bd
Tyler Gilchrist <tyler668@gmail.com>
2020-06-05 15:33:06 -0400

    MVP + 1

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,8 +3,20 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    cache = {}
+    posAndNeg = []
+    for i in a:
+        if i not in cache and -i not in cache:
+            cache[i] = i
+        else:
+            if i > 0:
+                posAndNeg.append(i)
+            else:
+                posAndNeg.append(-i)
+        
+        
 
-    return result
+    return posAndNeg
 
 
 if __name__ == "__main__":
